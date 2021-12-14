f18ada04e2044c4c9762cec3901c0e8dfdc40e9e
hsuehpe <bewitchsky@gmail.com>
2021-12-15 00:39:32 +0800

    feat: setup tailwindcss

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1El = screen.getByText(/Hello React/i);
+  expect(h1El).toBeInTheDocument();
 });
