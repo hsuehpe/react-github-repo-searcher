8fd1edb648bae9ff3da0c3621d751d9c11736861
hsuehpe <bewitchsky@gmail.com>
2021-12-16 16:13:38 +0800

    feat: add hooks

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function useDebounce(value: string, delay: number) {
-  const [debounceValue, setDebounceValue] = useState(value)
+  const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     let timer = setTimeout(() => setDebounceValue(value), delay);
     return () => clearTimeout(timer);
-  }, [value, delay])
+  }, [value, delay]);
   return debounceValue
-}
+};
--- src/hooks/useFetchRepos.ts
+++ src/hooks/useFetchRepos.ts
@@ -3,26 +3,39 @@ import { repoItem } from '../types/repoItem'
 import { getRepos } from '../api/search'
 
 export default function useFetchRepos(
-  query: string
+  query: string,
+  page: number
 ) {
-  const [isLoading, setIsLoading] = useState(false)
-  const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState<repoItem[] | []>([])
+  const [isQueryChanged, setIsQueryChanged] = useState(false)
+
+  const fetchData = async (p: number) => {
+    setIsLoading(true);
+    try {
+      const res = await getRepos(query, p);
+      setItems([...res.data.items]);
+    } catch (err) {
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    (async function fetchdata() {
-      setIsLoading(true);
-      try {
-        const res = await getRepos(query);
-        setItems([...res.data.items]);
-      } catch (err) {
-        setHasError(true);
-        setIsLoading(false);
-        return;
-      }
-      setIsLoading(false);
-    })()
+    if (query === '') {
+      setItems([]);
+    } else {
+      fetchData(1)
+    }
+
+    setIsQueryChanged(true)
   }, [query]);
 
-  return { isLoading, items, hasError }
+  useEffect(() => {
+    fetchData(page)
+    setIsQueryChanged(false)
+  }, [page])
+
+  return { isLoading, items, isQueryChanged }
 }
