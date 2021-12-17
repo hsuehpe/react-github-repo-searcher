bd213670875f1ac9135835e69bd57d37566b9c5a
hsuehpe <bewitchsky@gmail.com>
2021-12-18 01:19:38 +0800

    feat: implement request cancellation.

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,11 +1,14 @@
 import axios, { AxiosPromise } from 'axios';
 import { SEARCH_REPO_URL } from '../constants/github-api';
 
+let cancelTokenSource = axios.CancelToken.source();
+
 export function getRepos(query: string, page: number = 1): AxiosPromise<any> {
   return axios.get(SEARCH_REPO_URL, {
     headers: {
       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
+    cancelToken: cancelTokenSource.token,
     params: {
       q: query,
       sort: 'stars',
@@ -13,4 +16,9 @@ export function getRepos(query: string, page: number = 1): AxiosPromise<any> {
       page,
     },
   });
+}
+
+export function cancelRequest() {
+  cancelTokenSource.cancel();
+  cancelTokenSource = axios.CancelToken.source();
 }
--- src/hooks/useFetchRepos.ts
+++ src/hooks/useFetchRepos.ts
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
 import { repoItem } from '../types/repoItem'
 import { getRepos } from '../api/search'
+import { cancelRequest } from '../api/search';
 
 let page = 1;
 
@@ -26,7 +28,9 @@ export default function useFetchRepos(
   useEffect(() => {
     if (query === '') {
       setRepos([]);
+      cancelRequest();
     } else {
+      if (isLoading) return;
       page = 1
       fetchData(page, true)
     }
