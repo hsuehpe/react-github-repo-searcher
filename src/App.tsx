243949d844e1d72e850bc231d0dcdae1fc0126a6
hsuehpe <bewitchsky@gmail.com>
2021-12-20 00:24:15 +0800

    feat: use virtualized list

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import RepoList from './components/RepoList';
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center h-screen">
       <h1>Github Repositories Searcher</h1>
       <RepoList />
     </div>
